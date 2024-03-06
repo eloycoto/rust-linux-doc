db7d367c4e8662bad702b95c206f226f7c238af5
Eloy Coto <eloy.coto@acalustra.com>
2024-03-06 14:04:24 +0100

    next run
    Signed-off-by: Eloy Coto <eloy.coto@acalustra.com>

---
+++ script.sh
@@ -0,0 +1,13 @@
+
+curl -o linux.tar.gz -L https://git.kernel.org/torvalds/t/linux-6.8-rc7.tar.gz
+tar xzf linux.tar.gz
+cd linux-*
+make allnoconfig rust.config
+./scripts/config --disable GCC_PLUGINS
+./scripts/config --disable RANDSTRUCT
+#./scripts/config --disable CONFIG_HAVE_GCC_PLUGINS
+./scripts/config --enable RUST
+cat .config | grep "RUST"
+make rustavailable
+make rustdoc
+cat .config | grep "RUST"
