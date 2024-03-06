71f0b1821b2e4d33e626323a579736585653d6e6
Eloy Coto <eloy.coto@acalustra.com>
2024-03-06 16:06:22 +0100

    Fab Academy
    Signed-off-by: Eloy Coto <eloy.coto@acalustra.com>

--- script.sh
+++ script.sh
@@ -1,3 +1,4 @@
+set -e
 
 curl -o linux.tar.gz -L https://git.kernel.org/torvalds/t/linux-6.8-rc7.tar.gz
 tar xzf linux.tar.gz
@@ -5,9 +6,11 @@ cd linux-*
 make allnoconfig rust.config
 ./scripts/config --disable GCC_PLUGINS
 ./scripts/config --disable RANDSTRUCT
-#./scripts/config --disable CONFIG_HAVE_GCC_PLUGINS
+./scripts/config --disable CONFIG_HAVE_GCC_PLUGINS
 ./scripts/config --enable RUST
 ./scripts/config --enable HAVE_RUST
+./scripts/config --enable CONFIG_RUST
+./scripts/config --enable CONFIG_HAVE_RUST
 cat .config | grep "RUST"
 make rustavailable
 make rustdoc
