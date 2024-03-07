0ead5a3be5d796a9e828ca3a7c208fd7e9694e2d
Eloy Coto <eloy.coto@acalustra.com>
2024-03-07 09:40:49 +0100

    next run
    Signed-off-by: Eloy Coto <eloy.coto@acalustra.com>

--- script.sh
+++ script.sh
@@ -20,7 +20,7 @@ make allnoconfig rust.config defconfig
 ./scripts/config --enable CONFIG_RUST_OVERFLOW_CHECKS
 
 make rustavailable
-make rustdoc LLVM=1
+make rustdoc
 cat .config | grep "RUST"
 
 ls Documentation/output/ -lah
