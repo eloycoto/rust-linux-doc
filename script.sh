42a6a735747f861ac6a24fa8c83026abf9df579e
Eloy Coto <eloy.coto@acalustra.com>
2024-03-07 10:15:53 +0100

    next run
    Signed-off-by: Eloy Coto <eloy.coto@acalustra.com>

--- script.sh
+++ script.sh
@@ -1,14 +1,11 @@
 set -e
 
-#echo $LD_LIBRARY_PATH
-#env
-which ldd
+
 
 curl -o linux.tar.gz -L https://git.kernel.org/torvalds/t/linux-6.8-rc7.tar.gz
 tar xzf linux.tar.gz
 cd linux-*
 make allnoconfig rust.config defconfig
-#./scripts/config --enable GCC_PLUGINS
 ./scripts/config --disable RANDSTRUCT
 ./scripts/config --disable MODVERSIONS
 ./scripts/config --disable RANDSTRUCT
@@ -19,6 +16,7 @@ make allnoconfig rust.config defconfig
 ./scripts/config --enable CONFIG_HAVE_RUST
 ./scripts/config --enable CONFIG_RUST_OVERFLOW_CHECKS
 
+rustc --version
 make rustavailable
 make rustdoc CC=clang HOSTCC=gcc LD=ld.lld LLVM=1
 cat .config | grep "RUST"
